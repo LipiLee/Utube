84bac65413eb05090c88d58a064677699bffbd89
Lipi C.H. Lee <lipisoft@gmail.com>
2014-04-13 14:49:10 +0900

    Youtube Page is loaded.

--- app/src/main/java/com/lipisoft/android/utube/MainActivity.java
+++ app/src/main/java/com/lipisoft/android/utube/MainActivity.java
@@ -1,22 +1,21 @@
 package com.lipisoft.android.utube;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
-        Uri uri = Uri.parse("http://m.youtube.com");
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
+        WebView webview = new WebView(this);
+        webview.loadUrl("http://m.youtube.com/");
+        webview.getSettings().setJavaScriptEnabled(true);
+
+        setContentView(webview);
 
     }
 }
